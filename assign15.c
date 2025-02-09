e47e9a4015beb222f67a37a01db12a1462da5bce
SAURABH YADAV <saurabhyadav@SAURABHs-MacBook-Air.local>
2025-02-09 10:47:16 +0530

    add ques4,5

--- assign15.c
+++ assign15.c
@@ -1,5 +1,36 @@
 #include<stdio.h>
-void main()
+int main()
+{
+   int a,b,h;
+   printf("enter two number: ");
+   scanf("%d%d",&a,&b);
+   for(h=a<b?a:b;h<=a*b;h--)
+   {
+      if(a%h==0&&b%h==0)
+        break;
+   }
+   if(h==1)
+     printf("numbers are co prime");
+   else
+     printf("numbers are not co prime");
+
+   printf("\n");
+}
+int p4()
+{
+   int a,b,h;
+   printf("enter two number: ");
+   scanf("%d%d",&a,&b);
+   for(h=a<b?a:b;h<=a*b;h--)
+   {
+      if(a%h==0&&b%h==0)
+        break;
+   }
+   printf("hcf is %d",h);
+   printf("\n"); 
+
+}
+void p3()
 {
     int n,i;
     printf("enter a number: ");
