29c6b42e3354b92f9ef06bbd149b64cd6b310cb7
SAURABH YADAV <saurabhyadav@SAURABHs-MacBook-Air.local>
2025-09-16 10:10:02 +0530

    cprogramming

--- assignment4.c
+++ assignment4.c
@@ -1,84 +1,82 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int x,y;
+    int x, y;
     printf("enter two value: ");
-    scanf("%d%d",&x,&y);
-    y=x+y-(x=y);
-    printf("%d %d",x,y);
+    scanf("%d%d", &x, &y);
+    y = x + y - (x = y);
+    printf("%d %d", x, y);
     printf("\n");
     return 0;
 }
 int p7()
 {
-         int x,y,z;
+    int x, y, z;
     printf("enter two value: ");
-    scanf("%d%d",&x,&y);
-    z=x+y;
-    y=z-y;
-    x=z-x;
-    printf("%d %d",x,y);
+    scanf("%d%d", &x, &y);
+    z = x + y;
+    y = z - y;
+    x = z - x;
+    printf("%d %d", x, y);
     printf("\n");
     return 0;
 }
 int p6()
 {
-       int x,y;
+    int x, y;
     printf("enter two value: ");
-    scanf("%d%d",&x,&y);
-    x=x*y;
-    y=x/y;
-    x=x/y;
-    printf("%d %d",x,y);
+    scanf("%d%d", &x, &y);
+    x = x * y;
+    y = x / y;
+    x = x / y;
+    printf("%d %d", x, y);
     printf("\n");
     return 0;
 }
 int p5()
 {
-     int x,y;
+    int x, y;
     printf("enter two value: ");
-    scanf("%d%d",&x,&y);
-    x=x+y;
-    y=x-y;
-    x=x-y;
-    printf("%d %d",x,y);
+    scanf("%d%d", &x, &y);
+    x = x + y;
+    y = x - y;
+    x = x - y;
+    printf("%d %d", x, y);
     printf("\n");
     return 0;
 }
 int p4()
 {
-    int x,y,z;
+    int x, y, z;
     printf("enter two value: ");
-    scanf("%d%d",&x,&y);
-    z=x;
-    x=y;
-    y=z;
-    printf("%d %d",x,y);
+    scanf("%d%d", &x, &y);
+    z = x;
+    x = y;
+    y = z;
+    printf("%d %d", x, y);
     printf("\n");
     return 0;
 }
 
-
 int p3()
 {
-    char x='A';
+    char x = 'A';
     x++;
-    printf("%c",x);
+    printf("%c", x);
     printf("\n");
     return 0;
 }
 
 int p2()
-{ 
-  printf("size is %d",sizeof('4.5'));
-  printf("\n");
-  return 0; 
+{
+    printf("size is %d", sizeof('4.5'));
+    printf("\n");
+    return 0;
 }
-
 
 int p1()
 {
-  printf("size is %d",sizeof('A'));
-  printf("\n");
-  return 0;
+    printf("size is %d", sizeof('A'));
+    printf("\n");
+    return 0;
 }
